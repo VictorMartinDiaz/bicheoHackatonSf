c686a5aa92345749a9c58c1b1faba652c5bf9060
Victor Martin <victor.martin@cloudappi.net>
2021-11-18 09:43:02 +0100

    initial commit

---
+++ app/commerce-api.config.js
@@ -0,0 +1,9 @@
+export const commerceAPIConfig = {
+    proxyPath: `/mobify/proxy/api`,
+    parameters: {
+        clientId: '801b924e-ea52-4195-bd25-368e3b0aebc5',
+        organizationId: 'f_ecom_zzte_029',
+        shortCode: 'kv7kzm78',
+        siteId: 'RefArch'
+    }
+}
